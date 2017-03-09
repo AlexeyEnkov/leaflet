85e3166e9f41b762007b252d68552d9d11c5fab6
enkov <alexey.enkov@gmail.com>
2017-03-09 15:12:32 +0300

    generate content dynamically

--- src/index.kt
+++ src/index.kt
@@ -1,20 +1,43 @@
+@file:Suppress("PackageDirectoryMismatch")
+
 package ru.spb.enkov.leaflet
 
 import org.w3c.dom.HTMLElement
 import kotlin.browser.document
+import kotlin.browser.window
 
 external val L: dynamic
 
 fun main(args: Array<String>) {
-    val myMap = L.map("mapid").setView(arrayOf(51.505, -0.09), 13)
+    window.onload = { init() }
+}
 
+fun init() {
+    generateContent()
+
+    val map = L.map("mapId").setView(arrayOf(51.505, -0.09), 13)
 
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", object {
         val attribution = "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
-    }).addTo(myMap)
+    }).addTo(map)
 
-    val button = document.getElementById("button") as HTMLElement
-    button.onclick = {
-        console.log(myMap.getBounds())
+    val buttonDiv = document.getElementById("buttonId") as HTMLElement
+    buttonDiv.onclick = {
+        console.log(map.getBounds())
     }
+}
+
+fun generateContent() {
+    val mapDiv = document.createElement("div") as HTMLElement
+    mapDiv.id = "mapId"
+    mapDiv.style.height = "600px"
+
+    val buttonDiv = document.createElement("div") as HTMLElement
+    buttonDiv.id = "buttonId"
+    buttonDiv.innerText = "Button"
+    buttonDiv.style.color = "green"
+
+    val body = document.body!!
+    body.appendChild(mapDiv)
+    body.appendChild(buttonDiv)
 }
--- web/script/leaflet.js
+++ web/script/leaflet.js
@@ -3,30 +3,52 @@ if (typeof kotlin === 'undefined') {
 }
 var leaflet = function (_, Kotlin) {
   'use strict';
-  function main$ObjectLiteral() {
+  function main$lambda(it) {
+    init();
+  }
+  function main(args) {
+    window.onload = main$lambda;
+  }
+  function init$ObjectLiteral() {
     this.attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors';
   }
-  main$ObjectLiteral.$metadata$ = {
+  init$ObjectLiteral.$metadata$ = {
     kind: Kotlin.Kind.CLASS,
     interfaces: []
   };
-  function main$lambda(closure$myMap) {
+  function init$lambda(closure$map) {
     return function (it) {
-      console.log(closure$myMap.getBounds());
+      console.log(closure$map.getBounds());
     };
   }
-  function main(args) {
+  function init() {
     var tmp$;
-    var myMap = L.map('mapid').setView([51.505, -0.09], 13);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', new main$ObjectLiteral()).addTo(myMap);
-    var button = Kotlin.isType(tmp$ = document.getElementById('button'), HTMLElement) ? tmp$ : Kotlin.throwCCE();
-    button.onclick = main$lambda(myMap);
+    generateContent();
+    var map = L.map('mapId').setView([51.505, -0.09], 13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', new init$ObjectLiteral()).addTo(map);
+    var buttonDiv = Kotlin.isType(tmp$ = document.getElementById('buttonId'), HTMLElement) ? tmp$ : Kotlin.throwCCE();
+    buttonDiv.onclick = init$lambda(map);
+  }
+  function generateContent() {
+    var tmp$, tmp$_0, tmp$_1;
+    var mapDiv = Kotlin.isType(tmp$ = document.createElement('div'), HTMLElement) ? tmp$ : Kotlin.throwCCE();
+    mapDiv.id = 'mapId';
+    mapDiv.style.height = '600px';
+    var buttonDiv = Kotlin.isType(tmp$_0 = document.createElement('div'), HTMLElement) ? tmp$_0 : Kotlin.throwCCE();
+    buttonDiv.id = 'buttonId';
+    buttonDiv.innerText = 'Button';
+    buttonDiv.style.color = 'green';
+    var body = (tmp$_1 = document.body) != null ? tmp$_1 : Kotlin.throwNPE();
+    body.appendChild(mapDiv);
+    body.appendChild(buttonDiv);
   }
   var package$ru = _.ru || (_.ru = {});
   var package$spb = package$ru.spb || (package$ru.spb = {});
   var package$enkov = package$spb.enkov || (package$spb.enkov = {});
   var package$leaflet = package$enkov.leaflet || (package$enkov.leaflet = {});
   package$leaflet.main_kand9s$ = main;
+  package$leaflet.init = init;
+  package$leaflet.generateContent = generateContent;
   Kotlin.defineModule('leaflet', _);
   main([]);
   return _;
